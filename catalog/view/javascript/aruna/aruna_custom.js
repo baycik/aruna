17a843233cdac031833ebc633e4e45eb4c10a9f3
Baycik <Baycik@BaycikHome-tavrid390y7>
2018-11-29 22:28:42 +0300

    tried (but failed) to add SW

---
+++ catalog/view/javascript/aruna/aruna_custom.js
@@ -0,0 +1,12 @@
+if ( localStorage.getItem("disableSW")!=1 && ('serviceWorker' in navigator) ) {
+    navigator.serviceWorker.register('catalog/view/javascript/aruna/sw.js', {scope: './'})
+    .then(function(reg) {
+	console.log('serviceWorker Registration succeeded. Scope is ' + reg.scope);
+    }).catch(function(error) {
+	console.log('serviceWorker Registration failed with ' + error);
+    });
+    navigator.serviceWorker.addEventListener('message',function(event){
+	var msg=event.data;
+	console.log(msg);
+    });
+}
