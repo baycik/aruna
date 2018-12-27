7e354a0b02b224f7a44815ca3cdcc9bf607fda5d
Baycik <Baycik@BaycikHome-tavrid390y7>
2018-12-27 21:19:23 +0300

    bat files fixed

--- Modificators/iSellSoft-SellerSync.ocmod/iSellSoft-SellerSync.ocmod.bat
+++ Modificators/iSellSoft-SellerSync.ocmod/iSellSoft-SellerSync.ocmod.bat
@@ -1,9 +1,9 @@
 
 
-ROBOCOPY "../catalog/controller/extension/aruna/" "./iSellSoft-SellerSync.ocmod/upload/catalog/controller/extension/aruna/"
-ROBOCOPY "../catalog/model/extension/aruna/" "./iSellSoft-SellerSync.ocmod/upload/catalog/model/extension/aruna/"
-ROBOCOPY "../catalog/view/theme/default/template/extension/aruna/" "./iSellSoft-SellerSync.ocmod/upload/catalog/view/theme/default/template/extension/aruna/"
+ROBOCOPY "../../catalog/controller/extension/aruna/" "./upload/catalog/controller/extension/aruna/"  /mir
+ROBOCOPY "../../catalog/model/extension/aruna/" "./upload/catalog/model/extension/aruna/"  /mir
+ROBOCOPY "../../catalog/view/theme/default/template/extension/aruna/" "./upload/catalog/view/theme/default/template/extension/aruna/"  /mir
 
 
-"%ProgramFiles%\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y -x*.bat -x*.zip "./iSellSoft-SellerSync.ocmod.zip" "./*"
+"%ProgramFiles%\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y -x*.bat -x*.zip "../../../iSellSoft-SellerSync.ocmod.zip" "./*"
 PAUSE
