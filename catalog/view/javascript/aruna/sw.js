31e96475ea774bd4b0c64377e41a3709deb27af5
Baycik <Baycik@BaycikHome-tavrid390y7>
2019-01-01 15:37:16 +0300

    working on web application

--- catalog/view/javascript/aruna/sw.js
+++ catalog/view/javascript/aruna/sw.js
@@ -1,36 +1,50 @@
-	var cacheVersion="iSellSoftCache";
-	var autoupdate=1;
-	var resourcePattern=new RegExp(".+(.jpg|.jpeg|.png|.gif|.css|.js)$");
-	this.addEventListener('install', function (event) {
-	    console.log('iSellSoft Service Worker installed!!!');
+this.addEventListener('install', function (event) {
+    console.log('Aruna SW installed!!!');
+});
+
+this.addEventListener('message', function(event){
+    var message=event.data;
+    if( message==='clear_cache' ){
+	caches.delete('arunaCache').then(function(){
+	    console.log('arunaCache cleared!');
 	});
-	this.addEventListener('message', function(event){
-	    var message=event.data;
-	    if( message==='clear_cache' ){
-		caches.delete(cacheVersion).then(function(){
-		    console.log('iSellSoftCache cleared!');
-		});
-	    }
+    }
+});
+function postClientMessage( msg ){
+    clients.matchAll().then(function(clients){
+	clients.forEach(function(client){
+	    client.postMessage(msg);
 	});
-	this.addEventListener('fetch', function (event) {
-	    event.respondWith(
-		caches.match(event.request).then(function (cached_response) {
-		    if( cached_response && !autoupdate ){
-			return cached_response;
-		    }
-		    var fetched_response=fetch(event.request).then(function (response) {
-			if ( resourcePattern.test(event.request.url) ) {
-			    var resp2 = response.clone();
-			    caches.open('iSellSoftCache').then(function (cache) {
-				cache.put(event.request, resp2);
-			    });
-			}
-			return response;
-		    }).catch(function (e) {
-			console.log('isell-error',e);
-			return new Response('');
+    });
+}
+
+this.addEventListener('fetch', function (event) {
+    event.respondWith(
+	caches.match(event.request).then(function (cached_response) {
+	    var fetched_response=fetch(event.request).then(function (response) {
+		if( response.status!=200 ){
+		    postClientMessage( {
+			url: event.request.url,
+			status: response.status
 		    });
-		    return cached_response || fetched_response;
-		})
-	    );
-	});
+		} else 
+		if ( /[\w-_]+(.jpg|.jpeg|.png|.gif|.css|.js)/.test(event.request.url) ) {
+		    var resp2 = response.clone();
+		    caches.open('mobisellCache').then(function (cache) {
+			//console.log(" saved to mobisellCache: " + event.request.url);
+			cache.put(event.request, resp2);
+		    });
+		}
+		return response;
+	    }).catch(function (e) {
+		postClientMessage( {
+		    url: event.request.url,
+		    status: 408
+		});
+		console.log('isell-error',e);
+		return new Response('');
+	    });
+	    return cached_response || fetched_response;
+	})
+    );
+});
