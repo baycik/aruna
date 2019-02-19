fb8ad857050a87f79c385af5d3d0cbac5e450489
admin <admin@BaycikNote.Dlink>
2019-02-19 16:33:14 +0300

    sellerSync

--- catalog/controller/extension/aruna/sellersyncCron.php
+++ catalog/controller/extension/aruna/sellersyncCron.php
@@ -31,6 +31,18 @@ class ControllerExtensionArunaSellersyncCron extends Controller {
             'arguments'=>[2,5,null]
         ],
         [
+            'id'=>'charuttiParse',
+            'model'=>'extension/aruna/parse',
+            'method'=>'initParser',
+            'arguments'=>[8,'detect_unchanged_entries']
+        ],
+        [
+            'id'=>'charuttiImport',
+            'model'=>'extension/aruna/import',
+            'method'=>'importSellerProduct',
+            'arguments'=>[2,8,null]
+        ],
+        [
             'id'=>'isellParse',
             'model'=>'extension/aruna/parse',
             'method'=>'initParser',
@@ -41,6 +53,18 @@ class ControllerExtensionArunaSellersyncCron extends Controller {
             'model'=>'extension/aruna/import',
             'method'=>'importSellerProduct',
             'arguments'=>[20,7,null]
+        ],
+        [
+            'id'=>'deleteAbsentSellerProducts1',
+            'model'=>'extension/aruna/import',
+            'method'=>'deleteAbsentSellerProducts',
+            'arguments'=>[2]
+        ],
+        [
+            'id'=>'deleteAbsentSellerProducts2',
+            'model'=>'extension/aruna/import',
+            'method'=>'deleteAbsentSellerProducts',
+            'arguments'=>[20]
         ]
     ];
     public function index(){
